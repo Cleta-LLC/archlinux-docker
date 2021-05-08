5496a1ec5a906946e1f8307775310acb65c00495
Sergio David Munoz <sdmunozsierra@miners.utep.edu>
2021-05-08 13:49:47 -0400

    Created an command executor using ssh paramiko

--- python/base_installer_arch.py
+++ python/base_installer_arch.py
@@ -5,6 +5,92 @@ import paramiko
 from paramiko.channel import Channel
 from paramiko.ssh_exception import AuthenticationException
 
+class RaspiSSH():
+
+    def __init__(self, host, port, username, password=None):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.root_password = None
+        self.ssh = self._setup_ssh_connection()
+        self.channel = None
+        self.data = None
+    
+    def _setup_ssh_connection(self):
+        print("Setting up SSH with paramiko")
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        return ssh
+    
+    def add_password(self, password):
+        self.password = password
+    
+    def add_root_password(self, root_password):
+        self.root_password = root_password
+
+    def connect_channel(self):
+        print("Setting up a SSH connection")
+        if not self.password:
+            print("password is required to create a ssh connection.")
+            print("Set a password using add_password().")
+        try:
+            self.ssh.connect(self.host, self. port, self.username, self.password)
+            self.channel:Channel = self.ssh.invoke_shell()
+        except (timeout, AuthenticationException):
+            print("Could not connect to host")
+            exit(-1)
+    
+    def _obtain_su(self):
+        print("Using super user SU")
+        self.channel.send("su -\n")
+        time.sleep(1)
+        self.channel.send("\n".format(self.root_password))
+        time.sleep(2)
+
+    def _send_command(self, command, time_override=5, su=False, sudo=False):
+        if su and sudo:
+            print("Only use one super user command [su | sudo]")
+            return -1
+        if su:
+            print("Sending a super user su command")
+            self._obtain_su()
+            if isinstance(command, list):
+                for c in command:
+                    self.channel.send("{}\n".format(command))
+                    time.sleep(time_override)
+            else:
+                self.channel.send("{}\n".format(command))
+                time.sleep(time_override)
+            print("Terminating su session")
+            self.channel.send("{}\n".format("exit"))
+        elif sudo:
+            print("Sending a sudo command")
+            print("Not implemented yet")
+        else:
+            print("Sending a command")
+            if isinstance(command, list):
+                for c in command:
+                    self.channel.send("{}\n".format(command))
+                    time.sleep(time_override)
+            else:
+                self.channel.send("{}\n".format(command))
+                time.sleep(time_override)
+        if not self.data:
+            self.data = ""
+        self.data += str(self.channel.recv(999999))
+
+    def execute(self, command=None, time_override=5, su=False, sudo=False):
+        if not command:
+            print("Send a command to a Raspberry Pi through SSH")
+        print("Sending command: {}, waiting for {} seconds".format(command, time_override))
+        self._send_command(command, time_override, su, sudo)
+        print(self.data)
+
+    def example_command(self, command="whoami"):
+        self.execute(command=command)
+    
+
 def install_base(host, username, port, password, root_password="root"):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
--- python/fabfile.py
+++ python/fabfile.py
@@ -1,13 +1,100 @@
-import getpass
 from fabric import Connection, Config
 from invoke import Responder, watchers
+from base_installer_arch import RaspiSSH
 
 
-if __name__ == "__main__":
-    print("hello world from main")
-    #sudo_pass = getpass.getpass("Sudo password:")
-    #config = Config(overrides={'sudo': {'password': password}})
+class RaspiSSHCommander():
 
+    def __init__(self, host, username, port):
+        self.host = host
+        self.username = username
+        self.port = port
+
+    def obtain_su():
+        print("Using super user SU")
+        channel.send("su -\n")
+        time.sleep(1)
+        channel.send(root_password + "\n")
+        time.sleep(2)
+    
+    def exit_command():
+        print("Sending exit command")
+        channel.send("exit\n")
+        time.sleep(1)
+    
+    def populate_pacman():
+        # Run with su
+        print("Populating pacman this might take up to 6 minutes.")
+        channel.send("pacman-key --init\n")
+        time.sleep(3)
+        channel.send("pacman-key --populate archlinxarm\n")
+        time.sleep(5)
+
+    def update_pacman(command="pacman -Syu --noconfirm", time_override=360, su=True):
+        time.sleep(360)  # 6 minutes
+        self.channel.send("{}\n".format(command))
+
+    
+    def install_sudo():
+        # Run with su
+        print("Installing sudo")
+        channel.send("pacman -S sudo --noconfirm\n")
+        time.sleep(5)
+
+        print("Enabling sudoers. Uncommenting wheel")
+        channel.send("echo $'\n%wheel ALL=(ALL) ALL' >> /etc/sudoers\n")
+
+    def set_new_user(username="raspi", new_password="ipsar"):
+        obtain_su()
+
+        print("Setting up new user")
+        channel.send("useradd -m -g users -G wheel -s /bin/bash {}\n".format(username))
+        time.sleep(2)
+        channel.send("passwd {}\n".format(username))  # change passwd
+        time.sleep(1)
+        channel.send("{}\n".format(new_password))  # first time
+        time.sleep(2)
+        channel.send("{}\n".format(new_password))  # confirm passwd
+        time.sleep(1)
+
+        print("Activate sudo for new user")
+        exit_command()
+        channel.send("su {}\n".format(username))
+        time.sleep(3)
+        channel.send("{}\n".format(new_password))
+        time.sleep(3)
+
+    def receive_data(channel):
+        return channel.recv(999999)
+
+    def close_channel(channel):
+        channel.close()
+    
+
+def test_raspberry_update():
+    print("print test raspberry update")
+
+    # Connect one raspberry pi
+    raspi1 = RaspiSSH(host="192.168.1.2", port=22, username="alarm", password="alarm")
+    raspi1.add_root_password("root")
+    raspi1.connect_channel()
+
+    print("Expecting whoami command")
+    raspi1.example_command()
+
+    print("Sending populate keys")
+    raspi1.example_command(["pacman-key --init", "pacman-key --populate archlinuxarm"], su=True)
+
+    print("Sending update pacman")
+    raspi1.example_command(["pacman -Syu --noconfirm"], time_override=360, su=True)
+
+    # Bring the Swarm of Pi's
+    # raspi1.create_new_user("raspi01")
+    # raspi1.add_new_password("ipsar")
+
+
+def raspi_bridge():
+    """Here work commands that already have the users and groups already set up"""
     # Create dict with hostname and ip address
     fullhost = "alarm@192.168.1.243"
     user = "alarm"
@@ -18,8 +105,6 @@ if __name__ == "__main__":
 
     # Create a connection with the default Alarm user and password
     c = Connection("alarm@192.168.1.243", connect_kwargs={'password':password})
-    # c = Connection(host, user, port, connect_kwargs={'password':password}) # Same as above
-
     c.run('whoami')
 
     # Create a responder that will set up the Super User password 
@@ -27,9 +112,6 @@ if __name__ == "__main__":
         pattern=r'Password:',
         response='root\n',
     )
-
-    # Get Super User privileges
-    #c.run('su', pty=True, watchers=[responder_su])  # Now a superuser
 
     # Create a responder that will set up the sudo password 
     responder_su = Responder(
@@ -40,18 +122,6 @@ if __name__ == "__main__":
     # Run sudo commands
     c.run('sudo whoami', pty=True, watchers=[responder_su])
 
-
-    # Transpose bash script to be understandable by python
-    # 0. Super User setup the keyring
-    # 1. install sudo
-    # Use paramiko directly
-    # 2. Set Users & groups
-    # Use paramiko directly
-
-    # 3. `Disable` root user
-
-    # 4. Setup yay
-    # Create a new connection with new user
     # Finally sudo works as expected...
     config = Config(overrides={'sudo': {'password': 'toor'}})
     c = Connection("archnode@192.168.1.243", connect_kwargs={'password':'toor'}, config=config)
@@ -63,9 +133,28 @@ if __name__ == "__main__":
     c.run("git clone https://aur.archlinux.org/yay.git\n")
     c.run("cd yay && makepkg -si --noconfirm\n", pty=True, watchers="responder_password")
 
-    # Change the kernel if not already
-    # c.sudo("pacman -R linux-arch64 uboot-raspberrypi --noconfirm\n")
-    # c.sudo("pacman -S linux-raspberrypi4 --noconfirm\n")
+    # Install docker-machine
+    c.run("yay -S docker-machine --noconfirm\n", pty=True, watchers="responder_password")
 
-    # 5. Setup OBS
+
+if __name__ == "__main__":
+    print("hello world from main")
+
+    # 1. Connect to raspi first time and run a sample command whomai
+    # Connect one raspberry pi
+    print("Experiment 1: ")
+    raspi1 = RaspiSSH(host="192.168.1.2", port=22, username="alarm", password="alarm")
+    raspi1.connect_channel()
+
+    print("Expecting whoami command")
+    raspi1.example_command()
+    print("Complete!")
+
+    # raspi1.add_root_password("root")
+    # 0. Super User setup the keyring
+    # 1. install sudo # Use paramiko directly
+    # 2. Set Users & groups # Use paramiko directly
+    # 3. `Disable` root user
+    # 4. Setup yay
+
 
