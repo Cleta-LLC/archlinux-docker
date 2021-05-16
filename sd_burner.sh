00ab2318b7a72c30fae698126ecc2a698f6bf29d
Sergio David Munoz <sdmunozsierra@miners.utep.edu>
2021-05-16 13:58:27 -0400

    Added archlinux-arm-py as a submodule containing python scripts

--- sd_burner.sh
+++ sd_burner.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# Find which partitions to use automatically. #TODO: #Future #Request
-
-# Installation
-# Download and Install Arch
-export SDDEV=/dev/sdf
-export SDPARTBOOT=/dev/sdf1
-export SDPARTROOT=/dev/sdf2
-export SDMOUNT=/mnt/sd
-export DOWNLOADDIR=/tmp/pi
+# Download and Install Arch Linux Arm to a Rpi4-64
+# usage: ./sd_burner.sh /dev/sdc
 
 # Check if user is root or sudo
 if ! [ $( id -u ) = 0 ]; then
@@ -16,30 +8,51 @@ if ! [ $( id -u ) = 0 ]; then
     exit 1
 fi
 
+DEFAULT_PARTITION=/dev/sdx
+if [ "$1" ]; then
+  echo "Chose SD partition: $1"
+  DEFAULT_PARTITION=$1
+else
+  echo "No user argument using default value: ${DEFAULT_PARTITION}"
+fi
+
+# Export settings
+export SDDEV=${DEFAULT_PARTITION}
+export SDPARTBOOT=${SDDEV}1
+export SDPARTROOT=${SDDEV}2
+export SDMOUNT=/mnt/sd
+export DOWNLOADDIR=/tmp/pi
+echo -e -n "Settings:\nSDDEV=${SDDEV}\nBOOT=${SDPARTBOOT}\nROOT=${SDPARTROOT}\nSDMOUNT=${SDMOUNT}\nDOWNLOADDIR=${DOWNLOADDIR}\n"
+
+# Download. Never cache.
 mkdir -p $DOWNLOADDIR
 (
   cd $DOWNLOADDIR && \
   curl -JLO http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
 )
 
+# Clean disk
 sfdisk --quiet --wipe always $SDDEV << EOF
 ,256M,0c,
 ,,,
 EOF
-
+# Format disk
 mkfs.vfat -F 32 $SDPARTBOOT
 mkfs.ext4 -q -E lazy_itable_init=0,lazy_journal_init=0 -F $SDPARTROOT
 
+# Mount partitions
 mkdir -p $SDMOUNT
 mount $SDPARTROOT $SDMOUNT
 mkdir -p ${SDMOUNT}/boot
 mount $SDPARTBOOT ${SDMOUNT}/boot
 
+# Extract into SD
 bsdtar -xpf ${DOWNLOADDIR}/ArchLinuxARM-rpi-aarch64-latest.tar.gz -C $SDMOUNT
 
+# Change fstab
 sed -i 's/mmcblk0/mmcblk1/' ${SDMOUNT}/etc/fstab
 
-# (Optional for Headless Boot) Replace Uboot
+# Headless Boot - Replace Uboot
 mkdir -p ${DOWNLOADDIR}/uboot
 pushd ${DOWNLOADDIR}/uboot
 curl -JLO http://ports.ubuntu.com/pool/universe/u/u-boot/u-boot-rpi_2020.10+dfsg-1ubuntu0~20.04.2_arm64.deb
